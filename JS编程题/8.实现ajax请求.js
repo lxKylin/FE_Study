940987ec963b7de845fbabcebb67c850a68c79c0
liux <liux1118@qq.com>
2021-09-02 22:51:49 +0800

    手写ajax

---
+++ JS编程题/8.实现ajax请求.js
@@ -0,0 +1,24 @@
+// 创建XMLHttpRequest对象
+let xhr = new XMLHttpRequest();
+// 创建 Http 请求
+xhr.open("GET", "www.baidu.com", true);
+// 设置状态监听函数
+xhr.onreadystatechange = function() {
+  if (this.readyState !== 4) return;
+  // 当请求成功时
+  if (this.status === 200) {
+    handle(this.response);
+  } else {
+    console.error(this.statusText);
+  }
+};
+// 设置请求失败时的监听函数
+xhr.onerror = function() {
+  console.error(this.statusText);
+};
+// 设置请求头信息
+xhr.responseType = "json";
+xhr.setRequestHeader("Accept", "application/json");
+// 发送 Http 请求
+xhr.send(null);
+
