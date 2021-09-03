76ecfdcf7357e00dc1d5087edc5b15949c20da06
liux <liux1118@qq.com>
2021-09-03 10:12:54 +0800

    手写call及apply

---
+++ JS编程题/10.手写call、apply.js
@@ -0,0 +1,24 @@
+Function.prototype.myCall = function (context, ...args) {
+  // 判断有没有context或者context是否为空
+  if (!context || context === null) {
+    context = window;
+  }
+  // 创造唯一的key值  作为我们构造的context内部方法名
+  let fn = Symbol();
+  context[fn] = this; //this指向调用call的函数
+  // 执行函数并返回结果 相当于把自身作为传入的context的方法进行调用了
+  return context[fn](...args);
+};
+
+// apply原理一致  只是第二个参数是传入的数组
+Function.prototype.myApply = function (context, args) {
+  // 判断有没有context或者context是否为空
+  if (!context || context === null) {
+    context = window;
+  }
+  // 创造唯一的key值  作为我们构造的context内部方法名
+  let fn = Symbol();
+  context[fn] = this;
+  // 执行函数并返回结果
+  return context[fn](...args);
+};
