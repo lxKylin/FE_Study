5deac2d1a972d9165b0cf15a70487775d4ac18a4
liux <liux1118@qq.com>
2021-09-04 21:39:04 +0800

    二分查找

---
+++ JS编程题/12.二分查找.js
@@ -0,0 +1,28 @@
+// 如何确定一个数在一个有序数组中的位置
+function search(arr, target, start, end) {
+  let targetIndex = -1;
+
+  let mid = Math.floor((start + end) / 2);
+
+  if (arr[mid] === target) {
+    targetIndex = mid;
+    return targetIndex;
+  }
+
+  if (start >= end) {
+    return targetIndex;
+  }
+
+  if (arr[mid] < target) {
+    return search(arr, target, mid + 1, end);
+  } else {
+    return search(arr, target, start, mid - 1);
+  }
+}
+// const dataArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const position = search(dataArr, 6, 0, dataArr.length - 1);
+// if (position !== -1) {
+//   console.log(`目标元素在数组中的位置:${position}`);
+// } else {
+//   console.log("目标元素不在数组中");
+// }
