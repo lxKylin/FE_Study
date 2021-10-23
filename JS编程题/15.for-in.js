b5167c5555b05fcfb62d5b931861bc2a42a4c855
liux <liux1118@qq.com>
2021-10-23 22:40:18 +0800

    for-in输出key的顺序

---
+++ JS编程题/15.for-in.js
@@ -0,0 +1,35 @@
+// let arr = ['a', 'b', 'c']
+// arr["3"] = 'd'
+// arr["key"] = 'e'
+// arr[5] = 'f'
+
+// for (let key in arr) {
+//     console.log(key);
+// }
+// //0 1 2 3 5 key
+// console.log("======");
+
+// for (let value of arr) {
+//     console.log(value)
+// }
+// //a b c d underfined f
+// console.log("======");
+
+// let keys = Object.keys(arr);
+
+// console.log(keys);
+// //[ '0', '1', '2', '3', '5', 'key' ]
+let obj = {
+  2: 2,
+  b: 6,
+  7: 7,
+  a: 5,
+  56: 56,
+  10: 10,
+}
+for (let i in obj) {
+  console.log(i)
+}
+let k = Object.keys(obj)
+console.log(k)
+// [ '2', '7', '10', '56', 'b', 'a' ]
