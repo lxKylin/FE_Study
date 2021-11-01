38ae06183acc60cf7cf0deae72c38a4e7c4a836d
liux <liux1118@qq.com>
2021-11-01 09:30:21 +0800

    填充二维数组

--- JS编程题/18.创建一个二维数组方法.js
+++ JS编程题/18.创建一个二维数组方法.js
@@ -14,11 +14,18 @@ console.log(res)
 // empty表示空语句
 
 // 方式二
-const m = 4;
-const n = 5;
-let arr = Array(m).fill(0).map(() => Array(n));
+const m = 2;
+const n = 2;
+let as = [1, 2, 3, 4]
+// fill() 方法用一个固定值填充一个数组中从起始索引到终止索引内的全部元素
+// let arr = Array(m).fill().map(() => Array(n).fill());
+let arr = Array(m).fill().map(() => Array(n));
+for (let i = 0; i < as.length; i++) {
+  // ~~表示转数字，相当于Number()
+  arr[~~(i / n)][i % n] = as[i]
+}
 console.log(arr);
-//  Output: [ [ <5 empty items> ], [ <5 empty items> ], [ <5 empty items> ], , [ <5 empty items> ] ]
+// [ [ 1, 2 ], [ 3, 4 ] ]
 
 // 方式三
 const a = 4;
