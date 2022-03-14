daf15ed1ce489f9666d1beb905c1cb13f89aa245
kylin <liux1118@qq.com>
2022-03-14 12:35:42 +0800

    添加知识点

--- JS编程题/15.for-in.js
+++ JS编程题/15.for-in.js
@@ -19,17 +19,31 @@
 
 // console.log(keys);
 // //[ '0', '1', '2', '3', '5', 'key' ]
-let obj = {
-  2: 2,
-  b: 6,
-  7: 7,
-  a: 5,
-  56: 56,
-  10: 10,
-}
-for (let i in obj) {
-  console.log(i)
-}
-let k = Object.keys(obj)
-console.log(k)
+// let obj = {
+//   2: 2,
+//   b: 6,
+//   7: 7,
+//   a: 5,
+//   56: 56,
+//   10: 10,
+// }
+// for (let i in obj) {
+//   console.log(i)
+// }
+// let k = Object.keys(obj)
+// console.log(k)
 // [ '2', '7', '10', '56', 'b', 'a' ]
+
+let obj = {
+  5: 3,
+  bb: 3,
+  3: 4,
+  ba:4,
+  ab: 5,
+  length: 2,
+  push: Array.prototype.push
+}
+obj.push(1)
+obj.push(2)
+// obj.push(3)
+console.log(obj);
