6cf670325b6e31e0032970e3b467b851230a9a77
Kylin <1159469891@qq.com>
2024-09-10 15:19:07 +0800

    docs: 将字符串中的最长重复字母标记数量优化

--- JS编程题/29.将字符串中的最长重复字母标记数量.js
+++ JS编程题/29.将字符串中的最长重复字母标记数量.js
@@ -7,6 +7,7 @@
 function compressString(str) {
   let result = ''; // 结果字符串
   let count = 1; // 当前字符出现次数
+  let maxCount = 1;
 
   // 遍历字符串中的每个字符
   for (let i = 0; i < str.length; i++) {
@@ -14,8 +15,9 @@ function compressString(str) {
     if (str[i] === str[i + 1]) {
       count++;
     } else {
+      maxCount = Math.max(maxCount, count);
       // 否则，将当前字符及其计数添加到结果字符串中
-      result += count > 1 ? `${count}${str[i]}` : str[i];
+      result += count >= maxCount && count != 1 ? `${count}${str[i]}` : str[i];
       count = 1; // 重置计数
     }
   }
@@ -26,3 +28,4 @@ function compressString(str) {
 // 测试示例
 console.log(compressString('aaaa')); // 输出 "4a"
 console.log(compressString('abcaab')); // 输出 "abc2ab"
+console.log(compressString('abbbcaab')); // 输出 "a3bcab"
